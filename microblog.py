c5fe339f6b9f155631ab0b540c9a12cf70e009d2
digorcrispim <danieligorcrispim@gmail.com>
2021-03-18 18:47:46 +0000

    hello-word

--- microblog.py
+++ microblog.py
@@ -1,11 +1,1 @@
-from flask import Flask #ultimo Flask é escrito em maiusculo # antes de tudo pip install flask
-
-app = Flask("microblog")
-
-
-@app.route("/")  #linka a função index a URL # acessando a url o flask aciona a função index
-def index(): # função
-    return "Olá Mundo"
-
-
-app.run()
+from app import app
